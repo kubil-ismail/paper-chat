e9b1bcd7f01aab75e962a8bdd1095d597827f39f
Kubil Ismail <bilkisismail07@Gmail.com>
2021-02-18 14:19:19 +0700

    add enter code

--- src/components/Modal/EnterCode.tsx
+++ src/components/Modal/EnterCode.tsx
@@ -8,6 +8,8 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import Slide from "@material-ui/core/Slide";
 import TextField from "@material-ui/core/TextField";
 import { TransitionProps } from "@material-ui/core/transitions";
+import Firebase from "../../config/Firebase";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & { children?: React.ReactElement<any, any> },
@@ -17,6 +19,36 @@ const Transition = React.forwardRef(function Transition(
 });
 
 export default function EnterCode({ open, handleClose }) {
+  const [isLoading, setLoading] = React.useState(false);
+  const enterRoom = async (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+      setLoading(true);
+      // Get value input
+      const roomCode = e.target[2].value;
+      const userName = e.target[0].value;
+
+      // Connect to db
+      const connect = Firebase.database()
+        .ref(`/chats/${roomCode}`)
+        .orderByKey();
+
+      // Get value
+      connect.on("value", (snapshot) => {
+        const result = snapshot.val();
+        if (result) {
+          console.log(result);
+          setLoading(false);
+        } else {
+          console.log("no data");
+          setLoading(false);
+        }
+      });
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
       <Dialog
@@ -34,19 +66,49 @@ export default function EnterCode({ open, handleClose }) {
             <DialogContentText id="alert-dialog-slide-description">
               Masukan code room kamu
             </DialogContentText>
-            <TextField
-              autoFocus
-              id="name"
-              label="Room code"
-              type="text"
-              fullWidth
-              margin="dense"
-              variant="outlined"
-            />
-            <Box marginTop={3} />
-            <Button onClick={handleClose} variant="contained" fullWidth color="primary">
-              Enter
-            </Button>
+            <form onSubmit={enterRoom}>
+              <TextField
+                autoFocus
+                id="yourName"
+                label="Your Name"
+                type="text"
+                fullWidth
+                margin="dense"
+                variant="outlined"
+                required
+              />
+              <TextField
+                autoFocus
+                id="roomCode"
+                label="Room code"
+                type="text"
+                fullWidth
+                margin="dense"
+                variant="outlined"
+                required
+              />
+              <Box marginTop={3} />
+              <Button
+                variant="contained"
+                type="submit"
+                color="primary"
+                fullWidth
+                disabled={isLoading}
+              >
+                {isLoading ? (
+                  <>
+                    <CircularProgress
+                      color="secondary"
+                      size={15}
+                      style={{ marginRight: "10px" }}
+                    />
+                    Loading...
+                  </>
+                ) : (
+                  "Enter"
+                )}
+              </Button>
+            </form>
           </DialogContent>
         </Box>
       </Dialog>
